a9403a071ae66066fbddd15a1721de5cc2cfebbd
Li Zhineng <im@zhineng.li>
2024-07-24 20:30:23 +0800

    php-cs-fixer rule

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'no_unused_imports' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'integer_literal_case' => true,
+        'method_argument_space' => [
+            'on_multiline' => 'ignore',
+        ],
+        'ordered_imports' => [
+            'imports_order' => ['class', 'function', 'const'],
+            'sort_algorithm' => 'alpha',
+        ],
+    ])
+    ->setFinder($finder);

217d1a2f671dd4af94a2328f52c76916df335128
Li Zhineng <im@zhineng.li>
2024-08-15 09:02:02 +0800

    ignores property rule on instance exception

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -12,6 +13,9 @@ return RectorConfig::configure()
     ->withSkip([
         __DIR__.'/src/Tablestore/Messages',
         __DIR__.'/src/Tablestore/Metadata',
+        ClassPropertyAssignToConstructorPromotionRector::class => [
+            __DIR__.'/src/Tablestore/InstanceException.php',
+        ],
     ])
     ->withPhpSets()
     ->withTypeCoverageLevel(0);

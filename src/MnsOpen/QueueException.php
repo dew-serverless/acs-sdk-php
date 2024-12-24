e6b3ec053981cbca13df1362b66117661bec0989
Li Zhineng <im@zhineng.li>
2024-12-24 13:49:24 +0800

    multiple error messages

--- src/MnsOpen/QueueException.php
+++ src/MnsOpen/QueueException.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace Dew\Acs\MnsOpen;
 
 use Dew\Acs\AcsException;
+use Dew\Acs\Result;
+use Override;
 
+/**
+ * @phpstan-type TErrorsError array{ErrorCode: string, ErrorMessage: string}
+ */
 final class QueueException extends AcsException
 {
     /**
@@ -17,4 +22,48 @@ final class QueueException extends AcsException
      * The code field in the error response.
      */
     public const ERROR_CODE = 'Error.Code';
+
+    #[Override]
+    public static function makeFromResult(Result $result): static
+    {
+        if (! $result->has('Errors.Error')) {
+            return parent::makeFromResult($result);
+        }
+
+        return static::makeFromMultipleErrors($result);
+    }
+
+    /**
+     * Create an exception from multiple errors.
+     *
+     * @param  \Dew\Acs\Result<array<mixed>>  $result
+     */
+    private static function makeFromMultipleErrors(Result $result): static
+    {
+        /** @var TErrorsError[]|TErrorsError */
+        $errors = $result->get('Errors.Error');
+
+        if (! is_array($errors)) {
+            return parent::makeFromResult($result);
+        }
+
+        /** @var TErrorsError[] */
+        $errors = array_is_list($errors) ? $errors : [$errors];
+
+        $total = count($errors);
+
+        if ($total === 0) {
+            return parent::makeFromResult($result);
+        }
+
+        ['ErrorMessage' => $message, 'ErrorCode' => $code] = $errors[0];
+
+        $message = match ($total) {
+            1 => $message,
+            2 => $message.' (and 1 other)',
+            default => $message.sprintf(' (and %d others)', $total - 1),
+        };
+
+        return (new static($message, $code))->setResult($result);
+    }
 }
---
+++ tests/MnsOpen/QueueExceptionTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dew\Acs\Tests\MnsOpen;
+
+use Dew\Acs\MnsOpen\QueueException;
+use Dew\Acs\Result;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(QueueException::class)]
+final class QueueExceptionTest extends TestCase
+{
+    public function test_query_exception_should_be_thrown(): void
+    {
+        $e = QueueException::makeFromResult(new Result(['Error' => ['Code' => 'Unknown', 'Message' => 'Something went wrong!']]));
+        $this->assertSame('Something went wrong!', $e->getMessage());
+        $this->assertSame('Unknown', $e->getCode());
+    }
+
+    public function test_result_contains_multiple_error_messages_1_other(): void
+    {
+        $e = QueueException::makeFromResult(new Result([
+            'Errors' => [
+                'Error' => [
+                    ['ErrorCode' => 'Unknown1', 'ErrorMessage' => 'Something went wrong 1!'],
+                    ['ErrorCode' => 'Unknown2', 'ErrorMessage' => 'Something went wrong 2!'],
+                ],
+            ],
+        ]));
+        $this->assertSame('Something went wrong 1! (and 1 other)', $e->getMessage());
+        $this->assertSame('Unknown1', $e->getCode());
+    }
+
+    public function test_result_contains_multiple_error_messages_2_others(): void
+    {
+        $e = QueueException::makeFromResult(new Result([
+            'Errors' => [
+                'Error' => [
+                    ['ErrorCode' => 'Unknown1', 'ErrorMessage' => 'Something went wrong 1!'],
+                    ['ErrorCode' => 'Unknown2', 'ErrorMessage' => 'Something went wrong 2!'],
+                    ['ErrorCode' => 'Unknown3', 'ErrorMessage' => 'Something went wrong 3!'],
+                ],
+            ],
+        ]));
+        $this->assertSame('Something went wrong 1! (and 2 others)', $e->getMessage());
+        $this->assertSame('Unknown1', $e->getCode());
+    }
+
+    public function test_result_contains_multiple_error_messages_associative_array(): void
+    {
+        $e = QueueException::makeFromResult(new Result([
+            'Errors' => [
+                'Error' => [
+                    'ErrorCode' => 'Unknown1',
+                    'ErrorMessage' => 'Something went wrong 1!',
+                ],
+            ],
+        ]));
+        $this->assertSame('Something went wrong 1!', $e->getMessage());
+        $this->assertSame('Unknown1', $e->getCode());
+    }
+}

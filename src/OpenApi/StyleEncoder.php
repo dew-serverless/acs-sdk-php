6c5fb864ea21fa6ce9890bf3565046c691b23b5f
Li Zhineng <im@zhineng.li>
2024-12-28 17:21:35 +0800

    style encoder

---
+++ src/OpenApi/StyleEncoder.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dew\Acs\OpenApi;
+
+final class StyleEncoder
+{
+    /**
+     * Encode data into a JSON string.
+     *
+     * @param  array<mixed>  $data
+     */
+    public function encodeJson(array $data): string
+    {
+        return json_encode($data, JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * Encode data with "dot" notation.
+     *
+     * @param  array<mixed>  $data
+     * @return array<mixed>
+     */
+    public function encodeRepeatList(array $data, string $prefix = ''): array
+    {
+        $result = [];
+
+        foreach ($data as $key => $value) {
+            $slug = $prefix !== '' ? $prefix.'.' : '';
+            $slug .= is_int($key) ? $key + 1 : $key;
+
+            if (is_array($value)) {
+                $result = [...$result, ...$this->encodeRepeatList($value, $slug)];
+            } else {
+                $result[$slug] = $value;
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ tests/OpenApi/StyleEncoderTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dew\Acs\Tests\OpenApi;
+
+use Dew\Acs\OpenApi\StyleEncoder;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\TestWith;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(StyleEncoder::class)]
+final class StyleEncoderTest extends TestCase
+{
+    public function test_encode_json(): void
+    {
+        $encoder = new StyleEncoder();
+        $result = $encoder->encodeJson(['foo' => 'bar']);
+        $this->assertSame('{"foo":"bar"}', $result);
+    }
+
+    /**
+     * @param  array<string, mixed>  $data
+     * @param  array<string, mixed>  $expected
+     */
+    #[TestWith([['value' => [['name' => 'foo'], ['name' => 'bar']]], ['value.1.name' => 'foo', 'value.2.name' => 'bar']])]
+    #[TestWith([['value' => ['foo', 2, 3.14]], ['value.1' => 'foo', 'value.2' => 2, 'value.3' => 3.14]])]
+    #[TestWith([['foo', 2, 3.14], [1 => 'foo', 2 => 2, 3 => 3.14]])]
+    #[TestWith([['value' => ['foo' => 'bar']], ['value.foo' => 'bar']])]
+    #[TestWith([['value' => 'foo'], ['value' => 'foo']])]
+    public function test_encode_repeat_list(array $data, array $expected): void
+    {
+        $encoder = new StyleEncoder();
+        $result = $encoder->encodeRepeatList($data);
+        $this->assertSame($expected, $result);
+    }
+}

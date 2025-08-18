6a5ce84bdff6ceefb1a5f890934a3be351c5be35
Li Zhineng <im@zhineng.li>
2025-08-18 18:20:24 +0800

    remove legacy products

--- src/Schedulerx3/Schedulerx3Exception.php
+++ src/Schedulerx3/Schedulerx3Exception.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace Dew\Acs\Schedulerx3;
+namespace Dew\Acs\SchedulerX3;
 
 use Dew\Acs\AcsException;
 
-final class Schedulerx3Exception extends AcsException
+final class SchedulerX3Exception extends AcsException
 {
     //
 }
